0e53fec699af882bc65db7c4ebc55312f3937d28
eason777-li <2534850361@qq.com>
2026-09-07 15:22:31 +0800

    B: export TCP protocol client APIs

--- B_dispatch/__init__.py
+++ B_dispatch/__init__.py
@@ -1,18 +1,11 @@
-"""B EMS dispatch package."""
-
 from .dispatch import DispatchError, calculate_dispatch
 from .models import DispatchConfig, DispatchResult, GridState
 from .operator_core import EMSCore, EMSDecision
 from .runtime import EMSRuntime, RuntimeConfig
+from .tcp import Ack, EMSTcpClient, JsonLineFramer, ProtocolError, encode_frame, validate_envelope
 
 __all__ = [
-    "DispatchConfig",
-    "DispatchError",
-    "DispatchResult",
-    "EMSCore",
-    "EMSDecision",
-    "EMSRuntime",
-    "GridState",
-    "RuntimeConfig",
-    "calculate_dispatch",
+    "Ack", "DispatchConfig", "DispatchError", "DispatchResult", "EMSCore", "EMSDecision",
+    "EMSRuntime", "EMSTcpClient", "GridState", "JsonLineFramer", "ProtocolError", "RuntimeConfig",
+    "calculate_dispatch", "encode_frame", "validate_envelope",
 ]
