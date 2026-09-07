4e4b7b16966925747854b2b555e7b634bb4930c8
eason777-li <2534850361@qq.com>
2026-09-07 15:37:18 +0800

    B: export TCPB and serviceB layers

--- B_dispatch/__init__.py
+++ B_dispatch/__init__.py
@@ -2,10 +2,11 @@ from .dispatch import DispatchError, calculate_dispatch
 from .models import DispatchConfig, DispatchResult, GridState
 from .operator_core import EMSCore, EMSDecision
 from .runtime import EMSRuntime, RuntimeConfig
-from .tcp import Ack, EMSTcpClient, JsonLineFramer, ProtocolError, encode_frame, validate_envelope
+from .serviceB import EMSServiceB
+from .tcpB import Ack, EMSTcpClient, JsonLineFramer, ProtocolError, encode_frame, validate_envelope
 
 __all__ = [
     "Ack", "DispatchConfig", "DispatchError", "DispatchResult", "EMSCore", "EMSDecision",
-    "EMSRuntime", "EMSTcpClient", "GridState", "JsonLineFramer", "ProtocolError", "RuntimeConfig",
-    "calculate_dispatch", "encode_frame", "validate_envelope",
+    "EMSRuntime", "EMSServiceB", "EMSTcpClient", "GridState", "JsonLineFramer", "ProtocolError",
+    "RuntimeConfig", "calculate_dispatch", "encode_frame", "validate_envelope",
 ]
