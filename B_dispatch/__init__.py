4d81c43abc27873a502e4400cd74ab768fd31042
eason777-li <2534850361@qq.com>
2026-09-07 14:50:42 +0800

    feat: expose EMS runtime interfaces

--- B_dispatch/__init__.py
+++ B_dispatch/__init__.py
@@ -2,11 +2,17 @@
 
 from .dispatch import DispatchError, calculate_dispatch
 from .models import DispatchConfig, DispatchResult, GridState
+from .operator_core import EMSCore, EMSDecision
+from .runtime import EMSRuntime, RuntimeConfig
 
 __all__ = [
     "DispatchConfig",
     "DispatchError",
     "DispatchResult",
+    "EMSCore",
+    "EMSDecision",
+    "EMSRuntime",
     "GridState",
+    "RuntimeConfig",
     "calculate_dispatch",
 ]
